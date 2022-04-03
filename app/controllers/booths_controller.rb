a1028f56b9a29f10280ae655d8e78b2005dc7206
hardik <hardikvekariya09@gmail.com>
2022-04-03 15:23:52 +0530

    fixed: binding.pry removed

--- app/controllers/booths_controller.rb
+++ app/controllers/booths_controller.rb
@@ -7,13 +7,10 @@ class BoothsController < ApplicationController
   def create
     booth = Booth.create(booth_params)
     if booth.valid?
-      binding.pry
       flash[:notice] = "Employee is successfully added."
       customer = Stripe::Customer.create({ "email": current_user.email })
-      binding.pry
       c = customer.id
       d = booth.id
-      # @booking = Booking.create(email: current_user.email, stripe_id: c, event_id: params[:booth][:event_id], currency: "INR", stall_id: params[:booth][:stall_id])
       @booking = Booking.create!(email: current_user.email, stripe_id: c, event_id: params[:booth][:event_id], currency: "INR", stall_id: params[:booth][:stall_id].first, booth_id:booth.id)
 
       @stall = Stall.find(params[:booth][:stall_id].first).update(disable: true)
--- app/models/booth.rb
+++ app/models/booth.rb
@@ -2,5 +2,5 @@ class Booth < ApplicationRecord
   validates :first_name, :last_name, :email, :contact, presence: true
   # has_many :transactions
   validates :first_name, :last_name, length: { minimum: 2, maximum: 100 }
-  validates :contact, length: { is: 2 }
+  validates :contact, length: { is: 10 }
 end
